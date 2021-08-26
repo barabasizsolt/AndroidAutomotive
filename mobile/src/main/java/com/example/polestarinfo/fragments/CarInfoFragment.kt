fd38ea6c697d969d2b39b0e37a1a293573f874c6
barabasi_zsolt <barabasi.r.zsolt@accenture.com>
2021-08-26 14:18:53 +0300

    carFragment UI design added.

--- mobile/src/main/java/com/example/polestarinfo/fragments/CarInfoFragment.kt
+++ mobile/src/main/java/com/example/polestarinfo/fragments/CarInfoFragment.kt
@@ -1,7 +1,6 @@
 package com.example.polestarinfo.fragments
 
 import android.content.Context
-import android.content.Context.SENSOR_SERVICE
 import android.hardware.Sensor
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -9,17 +8,18 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.polestarinfo.R
-import androidx.core.content.ContextCompat.getSystemService
-
 import android.hardware.SensorManager
 import android.util.Log
-import androidx.core.content.ContextCompat
+import com.google.android.material.tabs.TabLayout
+
+
 
 
 class CarInfoFragment : Fragment() {
+    private lateinit var tabLayout: TabLayout
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
     }
 
     override fun onCreateView(
@@ -27,6 +27,11 @@ class CarInfoFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         val view = inflater.inflate(R.layout.fragment_car_info, container, false)
+
+        tabLayout = view.findViewById(R.id.tab_layout)
+        for (i in 0 until tabLayout.tabCount) {
+            tabLayout.getTabAt(i)?.setCustomView(R.layout.custom_tab_layout)
+        }
 
         val sensorManager = activity?.getSystemService(Context.SENSOR_SERVICE) as SensorManager
         val deviceSensors: List<Sensor> = sensorManager.getSensorList(Sensor.TYPE_ALL)
